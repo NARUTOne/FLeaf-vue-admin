63eb67695f4904d26d592e11a543cd8854fa3cfc
NARUTOne <wznaruto326@163.com>
2018-04-14 15:57:08 +0800

    some git test

---
+++ src/pages/layout/rem/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="layout-rem"></div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutRem'
+};
+</script>
+
+<style lang="less" scoped>
+.layout-rem {
+  
+}
+</style>
+
+
