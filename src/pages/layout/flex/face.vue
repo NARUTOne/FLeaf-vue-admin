e1b4cd746a96632d47e607826fac8fdddc749bbc
NARUTOne <wznaruto326@163.com>
2018-06-13 18:53:17 +0800

    flex layout

---
+++ src/pages/layout/flex/face.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="flex-face flex box default-bg">
+    <div class="face flex first-pip">
+			<span class="pip"></span>
+		</div>
+		<div class="face flex second-pip">
+			<span class="pip"></span>
+			<span class="pip"></span>
+		</div>
+		<div class="face flex three-pip">
+			<span class="pip"></span>
+			<span class="pip"></span>
+			<span class="pip"></span>
+		</div>
+		<div class="face flex four-pip">
+			<div class="column">
+		    <span class="pip"></span>
+		    <span class="pip"></span>
+		  </div>
+		  <div class="column">
+		    <span class="pip"></span>
+		    <span class="pip"></span>
+		  </div>
+		</div>
+		<div class="face flex five-pip">
+			<div class="column">
+		    <span class="pip"></span>
+		    <span class="pip"></span>
+		  </div>
+		  <div class="column">
+		    <span class="pip"></span>
+		  </div>
+		  <div class="column">
+		    <span class="pip"></span>
+		    <span class="pip"></span>
+		  </div>
+		</div>
+		<div class="face flex six-pip">
+			<div class="column">
+		    <span class="pip"></span>
+		    <span class="pip"></span>
+		    <span class="pip"></span>
+		  </div>
+		  <div class="column">
+		  	<span class="pip"></span>
+		    <span class="pip"></span>
+		    <span class="pip"></span>
+		  </div>
+		</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FPip'
+};
+</script>
+
+<style lang="less" scoped>
+.flex {
+	display: flex;
+	display: -webkit-flex;
+}
+
+.box {
+	padding: 8px 16px;
+	border-radius: 6px;
+	justify-content: space-around;
+}
+
+.face {
+	margin: 16px;
+  padding: 4px;
+  background-color: #e7e7e7;
+  width: 104px;
+  height: 104px;
+  -o-object-fit: contain;
+  object-fit: contain;
+  box-shadow: inset 0 5px white, inset 0 -5px #bbb, inset 5px 0 #d7d7d7, inset -5px 0 #d7d7d7;
+  border-radius: 10%;
+}
+
+.pip {
+	display: inline-block;
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  margin: 4px;
+  background-color: #333;
+  box-shadow: inset 0 3px #111, inset 0 -3px #555;
+}
+
+.first-pip {
+	justify-content: center;
+	align-items: center;
+}
+
+
+.second-pip {
+	justify-content: space-between;
+}
+
+.second-pip > .pip:nth-child(2) {
+	align-self: flex-end;
+}
+
+.three-pip > .pip:nth-child(2) {
+	align-self: center;
+}
+
+.three-pip > .pip:nth-child(3) {
+	align-self: flex-end;
+}
+
+.four-pip {
+  flex-wrap: wrap;
+  align-content: space-between;
+}
+.column {
+  flex-basis: 100%;
+  display: flex;
+  justify-content: space-between;
+}
+
+.five-pip {
+  flex-wrap: wrap;
+  align-content: space-between;
+}
+.five-pip > .column:nth-child(2) {
+	justify-content: center;
+}
+
+.six-pip {	
+	justify-content: space-between;
+}
+
+.six-pip > .column {
+	flex-basis: auto;
+	flex-direction: column;
+	justify-content: space-between;
+}
+</style>
+
---
+++ src/pages/layout/flex/layout.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="flex-layout">
+    <div class="page">
+			<div class="header default-bg">header</div>
+			<div class="body">
+        <div class="sider success-bg">sider</div>
+        <div class="content warn-bg"> content</div>
+      </div>
+			<div class="footer default-bg">footer</div>
+		</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FLayout'
+};
+</script>
+
+<style lang="less" scoped>
+  .flex-layout {
+    display: flex;
+  }
+  .page {
+    width:100%;
+    height:50vh;
+    display:-webkit-flex;
+    display:flex;
+    -webkit-flex-direction:column;
+    flex-direction:column;
+    margin:0 auto; 
+    padding:0 20px;
+  }
+	.header, .content, .sider { 
+    padding:20px; 
+  }
+	.footer { 
+    padding:40px 20px; 
+  }
+  .body { 
+    display: flex;
+    -webkit-flex-grow:1;
+    flex-grow:1;
+    flex-direction: row;
+  }
+  .sider {
+    width: 200px;
+    flex: 0 0 200;
+  }
+
+  .content {
+    flex: 1;
+  }
+
+</style>
+
