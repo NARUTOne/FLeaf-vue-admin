977cdf1b85edf6b18caa0045f8e7ae3ab9c7deb7
NARUTOne <wznaruto326@163.com>
2018-07-09 20:16:25 +0800

    admin

--- src/index.js
+++ src/index.js
@@ -24,6 +24,24 @@ Vue.prototype.$Message = Message;
 Vue.prototype.$FLVMessage = FLVMessage;
 Vue.prototype.$Loading = LoadingBar;
 
+// 指令
+// 注册一个全局自定义指令 `v-focus`
+Vue.directive('focus', {
+  // 当被绑定的元素插入到 DOM 中时……
+  inserted: function (el) {
+    // 聚焦元素
+    el.focus();
+  }
+});
+
+// 过滤
+Vue.filter('toNum', function (arr) {
+  return arr.map(item => {
+    item = item - 0;
+    return item;
+  });
+});
+
 new Vue({
   el: '#app',
   router,
